fb7c06b4f9de8f6d3d131b1305993dd2ed12d8ff
qpzm <lhm1442@gmail.com>
2021-11-26 15:19:16 +0900

    backend: Add test for Staker contract

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity 0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
@@ -7,25 +7,83 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  mapping(address => uint256) public balances;
+
+  bool public isActive = false;
+  uint256 constant public threshold = 0.003 * 10 ** 18;
+  uint256 public deadline = block.timestamp + 2 minutes;
+
+  event Stake(address indexed sender, uint256 amount);
+  event Withdraw(address indexed sender, uint256 amount);
+
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+  modifier deadlineReached() {
+    uint256 timeRemaining = timeLeft();
+    require(timeRemaining == 0, "Deadline is not reached yet");
+    _;
+  }
+
+  modifier deadlineRemaining() {
+    uint256 timeRemaining = timeLeft();
+    require(timeRemaining > 0, "Deadline is already reached");
+    _;
+  }
+
+  /**
+  * @notice Modifier that require the external contract to not be completed
+  */
+  modifier stakeNotCompleted() {
+    bool completed = exampleExternalContract.completed();
+    require(!completed, "staking process already completed");
+    _;
+  }
+
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable {
+    balances[msg.sender] += msg.value;
 
+    if (block.timestamp <= deadline && address(this).balance >= threshold) {
+      isActive = true;
+    }
+
+    emit Stake(msg.sender, msg.value);
+  }
+
+  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+  function withdraw() public deadlineReached stakeNotCompleted {
+    require(block.timestamp > deadline, "deadline hasn't passed yet");
+    require(isActive == false, "Contract is active");
+    require(balances[msg.sender] > 0, "You haven't deposited");
+
+    uint256 amount = balances[msg.sender];
+    balances[msg.sender] = 0;
+
+    (bool sent, ) = msg.sender.call{value: amount}("");
+    require(sent, "Failed to send user balance back to the user");
+
+    emit Withdraw(msg.sender, amount);
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  function execute() public stakeNotCompleted deadlineRemaining {
+    require(block.timestamp > deadline, "deadline hasn't passed yet");
+    require(address(this).balance >= threshold);
 
-
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    exampleExternalContract.complete{value: address(this).balance}();
+  }
 
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
-
+  function timeLeft() public view returns (uint256 timeleft) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return deadline - block.timestamp;
+    }
+  }
 }
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -4,23 +4,221 @@ const { solidity } = require("ethereum-waffle");
 
 use(solidity);
 
-describe("My Dapp", function () {
-  let myContract;
+const increaseWorldTimeInSeconds = async (seconds, mine) => {
+  await ethers.provider.send("evm_increaseTime", [seconds]);
+  if (mine) {
+    await ethers.provider.send("evm_mine", []);
+  }
+};
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
+describe("Staker Dapp", function () {
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-      myContract = await YourContract.deploy();
+  let stakerContract;
+  let exampleExternalContract;
+  let ExampleExternalContractFactory;
+
+  beforeEach(async () => {
+    // Deploy ExampleExternalContract contract
+    ExampleExternalContractFactory = await ethers.getContractFactory(
+      "ExampleExternalContract"
+    );
+    exampleExternalContract = await ExampleExternalContractFactory.deploy();
+
+    // Deploy Staker Contract
+    const StakerContract = await ethers.getContractFactory("Staker");
+    stakerContract = await StakerContract.deploy(
+      exampleExternalContract.address
+    );
+
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+  });
+
+  describe("Test contract utils methods", () => {
+    it("timeLeft() return 0 after deadline", async () => {
+      await increaseWorldTimeInSeconds(180, true);
+
+      const timeLeft = await stakerContract.timeLeft();
+      expect(timeLeft).to.equal(0);
     });
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
+    it("timeLeft() return correct timeleft after 10 seconds", async () => {
+      const secondsElapsed = 10;
+      const timeLeftBefore = await stakerContract.timeLeft();
+      await increaseWorldTimeInSeconds(secondsElapsed, true);
 
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
+      const timeLeft = await stakerContract.timeLeft();
+      expect(timeLeft).to.equal(timeLeftBefore.sub(secondsElapsed));
+    });
+  });
+
+  describe("Test stake() method", () => {
+    it("Stake event emitted", async () => {
+      const amount = ethers.utils.parseEther("0.5");
+      await expect(stakerContract.connect(addr1).stake({ value: amount }))
+        .to.emit(stakerContract, "Stake")
+        .withArgs(addr1.address, amount);
+    });
+
+    it("Stake 0.5 ETH from single user", async () => {
+      const amount = ethers.utils.parseEther("0.5");
+      const tx = await stakerContract.connect(addr1).stake({
+        value: amount,
       });
+      await tx.wait();
+
+      // Check that the contract has the correct amount of ETH we just sent
+      const contractBalance = await ethers.provider.getBalance(
+        stakerContract.address
+      );
+      expect(contractBalance).to.equal(amount);
+
+      // Check that the contract has stored in our balances state the correct amount
+      const addr1Balance = await stakerContract.balances(addr1.address);
+      expect(addr1Balance).to.equal(amount);
+    });
+
+    it("Stake reverted if deadline is reached", async () => {
+      // Let deadline be reached
+      await increaseWorldTimeInSeconds(180, true);
+
+      const amount = ethers.utils.parseEther("0.5");
+      await expect(
+        stakerContract.connect(addr1).stake({
+          value: amount,
+        })
+      ).to.be.revertedWith("Deadline is already reached");
+      // Complete the stake process
+    });
+
+    it("Stake reverted if external contract is completed", async () => {
+      const amount = ethers.utils.parseEther("1");
+      // Complete the stake process
+      const txStake = await stakerContract.connect(addr1).stake({
+        value: amount,
+      });
+      await txStake.wait();
+
+      // Execute it
+      const txExecute = await StakerContract.connect(addr1).execute();
+      await txExecute.wait();
+
+      await expect(
+        stakerContract.connect(addr1).stake({
+          value: amount,
+        })
+      ).to.be.revertedWith("staking process already completed");
+    });
+  });
+
+  describe("Test execute() method", () => {
+    it("execute reverted because stake amount not reached threshold", async () => {
+      await expect(stakerContract.connect(addr1).execute()).to.be.revertedWith(
+        "Threshold not reached"
+      );
+    });
+
+    it("execute reverted because external contract already completed", async () => {
+      const amount = ethers.utils.parseEther("1");
+      await StakerContract.connect(addr1).stake({
+        value: amount,
+      });
+      await stakerContract.connect(addr1).execute();
+
+      await expect(stakerContract.connect(addr1).execute()).to.be.revertedWith(
+        "staking process already completed"
+      );
+    });
+
+    it("execute reverted because deadline is reached", async () => {
+      // Let deadline be reached
+      await increaseWorldTimeInSeconds(180, true);
+
+      await expect(stakerContract.connect(addr1).execute()).to.be.revertedWith(
+        "Deadline is already reached"
+      );
+    });
+
+    it("external contract successfully completed", async () => {
+      const amount = ethers.utils.parseEther("1");
+      await stakerContract.connect(addr1).stake({
+        value: amount,
+      });
+      await stakerContract.connect(addr1).execute();
+
+      // it seems to be a waffle bug see https://github.com/EthWorks/Waffle/issues/469
+      // test that our Stake Contract has successfully called the external contract's complete function
+      // expect('complete').to.be.calledOnContract(exampleExternalContract);
+
+      const completed = await exampleExternalContract.completed();
+      expect(completed).to.equal(true);
+
+      const externalContractBalance = await ethers.provider.getBalance(
+        exampleExternalContract.address
+      );
+      expect(externalContractBalance).to.equal(amount);
+
+      const contractBalance = await ethers.provider.getBalance(
+        stakerContract.address
+      );
+      expect(contractBalance).to.equal(0);
+    });
+  });
+
+  describe("Test withdraw() method", () => {
+    it("withdraw reverted if deadline is not reached", async () => {
+      await expect(
+        stakerContract.connect(addr1).withdraw(addr1.address)
+      ).to.be.revertedWith("Deadline is not reached yet");
+    });
+
+    it("withdraw reverted if external contract is completed", async () => {
+      const txStake = await stakerContract.connect(addr1).stake({});
+      await txStake.wait();
+
+      const txExecute = await stakerContract.connect(addr1).execute();
+      await txExecute.wait();
+
+      await increaseWorldTimeInSeconds(180, true);
+
+      await expect(
+        stakerContract.connect(addr1).withdraw(addr1.address)
+      ).to.be.revertedWith("staking process already completed");
+    });
+
+    it("withdraw reverted if address has no balance", async () => {
+      await increaseWorldTimeInSeconds(180, true);
+
+      await expect(
+        stakerContract.connect(addr1).withdraw(addr1.address)
+      ).to.be.revertedWith("You don't have balance to withdraw");
+    });
+
+    it("withdraw success!", async () => {
+      // Complete the stake process
+      const amount = ethers.utils.parseEther("1");
+      const txStake = await stakerContract.connect(addr1).stake({
+        value: amount,
+      });
+      await txStake.wait();
+
+      // Let time pass
+      await increaseWorldTimeInSeconds(180, true);
+
+      const txWithdraw = await stakerContract
+        .connect(addr1)
+        .withdraw(addr1.address);
+      await txWithdraw.wait();
+
+      const contractBalance = await ethers.provider.getBalance(
+        stakerContract.address
+      );
+      expect(contractBalance).to.equal(0);
+
+      await expect(txWithdraw).to.changeEtherBalance(addr1, amount);
     });
   });
 });
